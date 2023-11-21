de7ec5e255107709929167a3656806b4ddb7e80a
Masoom Rizvi <masoom2313189@gmail.com>
2023-11-21 19:05:05 +0530

    adding

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
